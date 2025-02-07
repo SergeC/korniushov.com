e82362593ef9923fd610a06caf91bcebdada03ef
serg <you@example.com>
2025-02-06 22:25:49 -0600

    add rss feed v1

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 // https://nextjs.org/docs/app/api-reference/file-conventions/metadata/sitemap
 import type {MetadataRoute} from 'next'
-import {getAllPosts, Module} from "@/lib/blog";
+import {getAllPosts, Module, Post} from "@/lib/blog";
+import {authorName, siteDescription} from "@/lib/consts";
+import {writeFileSync} from "fs";
 
 export const dynamic = "force-static";
 
@@ -29,9 +31,15 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       });
   }
 
+  // RSS for blog post & snippet
+  if (posts.length > 0) {
+    const rss = generateRss(posts);
+    writeFileSync('./public/feed.xml', rss)
+  }
+
   // return sitemap array
   return uris.map((uri) => {
-    uri.url = getFulUrl(uri.url);
+    uri.url = getFullUrl(uri.url);
     if (!('lastModified' in uri)) {
       uri.lastModified = new Date();
     }
@@ -55,6 +63,37 @@ enum changefreq {
   never = 'never',
 }
 
-function getFulUrl(path: string): string {
+function getFullUrl(path: string): string {
   return `${process.env.NEXT_PUBLIC_FRONTEND_HOST}${path}`
+}
+
+function generateRssItem(post: Post) {
+  return `
+    <item>
+      <guid>${getFullUrl(`/blog/${post.slug}`)}</guid>
+      <title>${post.title}</title>
+      <link>${getFullUrl(`/blog/${post.slug}`)}</link>
+      ${post.excerpt && `<description>${post.excerpt}</description>`}
+      <pubDate>${new Date(post.createdAt).toUTCString()}</pubDate>
+      <author>${authorName}</author>
+    </item>
+	`
+}
+
+function generateRss(posts: Post[]) {
+  return `
+    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+      <channel>
+        <title>${authorName}'s Blog</title>
+        <link>${getFullUrl(`/blog`)}</link>
+        <description>${siteDescription}</description>
+        <language>en-us</language>
+        <managingEditor>${authorName}</managingEditor>
+        <webMaster>${authorName}</webMaster>
+        <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+        <atom:link href="${getFullUrl(`/feed.xml`)}" rel="self" type="application/rss+xml"/>
+        ${posts.map((post) => generateRssItem(post)).join('')}
+      </channel>
+    </rss>
+	`
 }
--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -2,8 +2,10 @@ export const authorName = 'Serhii Korniushov';
 export const authorPhoto = 'https://pbs.twimg.com/profile_images/686928492496306176/d3ipJ7SA_400x400.jpg';
 export const authorOccupation = 'Lead Software Engineer';
 export const authorLocation = 'Austin, TX';
-export const siteKeywords = ['NEMT', 'non-emergency medical transportation', 'services', 'marketplace'];
+export const siteKeywords = [authorName, 'software development', 'software engineering', 'symfony', 'php', 'fintech', 'artificial intelligence'];
+export const siteDescription = `${authorName}, a ${authorOccupation} with 10+ years of experience building scalable systems, primarily in PHP`;
 
-export const twitterURL = 'https://x.com/scornushov';
+export const twitterUsername = 'scornushov';
+export const twitterURL = `https://x.com/${twitterUsername}`;
 export const githubURL = 'https://github.com/SergeC';
 export const linkedinURL = 'https://www.linkedin.com/in/serhii-korniushov/';
